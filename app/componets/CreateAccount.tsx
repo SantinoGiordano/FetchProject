9ac5afdc146db67d62c5957581f01d7605a1f5a2
SantinoGiordano <santinogiordano13@gmail.com>
2024-12-18 11:25:33 -0500

    created start for page

---
+++ app/componets/CreateAccount.tsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+export function CreateAccount() {
+  const [passWord, setPassword] = useState([]);
+  const [username, setUsername] = useState([]);
+  const [age, setAge] = useState([]);
+  const [email, setEmail] = useState([]);
+  const [name, setName] = useState([]);
+}
