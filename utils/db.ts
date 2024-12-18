08123c4b66202e5eb8ece7f9d3d747253aa95659
SantinoGiordano <santinogiordano13@gmail.com>
2024-12-18 13:39:06 -0500

    Got rid of id and now post is a 400

--- utils/db.ts
+++ utils/db.ts
@@ -10,14 +10,14 @@ async function dbConnect(){
     if(mongoose.connection.readyState !==1){
         try {
             await mongoose.connect(MONGO_URI)
-            console.log("db is connected")
+            console.log("DB is connected")
         } catch (error) {
             console.log(error)
             throw error
         }
     }
     else{
-        console.log("DB is alreaddy connected")
+        console.log("DB is already connected")
     }
 
 }
