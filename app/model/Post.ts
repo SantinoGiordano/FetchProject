589c2a5237d8dca1142ae4820e29fff0ff28aa13
SantinoGiordano <santinogiordano13@gmail.com>
2024-12-18 19:26:42 -0500

    created posts Schema

---
+++ app/model/Post.ts
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose'
+
+export interface IPost extends Document {
+   title:string;
+   description:string;
+   img:string;
+   likes:number;
+  }
+  
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+  },
+  description:{
+    type:String,
+  },
+  img:{
+    type:String
+  },
+  like:{
+    type:Number
+  },
+  created_at: {
+    type: Date,
+    default: Date.now(),
+  },
+  updated_at: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const Post = mongoose.models.post || mongoose.model("post", postSchema);
+
+export default Post;
