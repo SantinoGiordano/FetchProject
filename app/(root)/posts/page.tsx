51d28f3f2d26c62fef786fa38fe37f34e1be2852
SantinoGiordano <santinogiordano13@gmail.com>
2024-12-20 11:56:25 -0500

    added a breaker page to decide what you want to do - create or view

--- app/(root)/posts/page.tsx
+++ app/(root)/posts/page.tsx
@@ -1,12 +1,30 @@
-'use client'
+"use client";
 
-import React from 'react'
-import { CreatePost } from '../componets/CreatePost'
+import React from "react";
+import Link from "next/link";
 
 const page = () => {
   return (
-    <CreatePost/>
-  )
-}
+    <div className="text-4xl bg-green-50">
+      <div className="flex items-center justify-center min-h-screen">
+        <div className="flex space-x-2">
+          <Link
+            href={"./view"}
+            className="font-bold hover:scale-110 transition-transform"
+          >
+            View
+          </Link>
+          <span>Or</span>
+          <Link
+            href={"./posts/createpost"}
+            className="font-bold hover:scale-110 transition-transform"
+          >
+            Create
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default page;
