fdabe2d798849caf46789f35c63b031679c2697c
SantinoGiordano <santinogiordano13@gmail.com>
2024-12-20 11:56:44 -0500

    imported componet

---
+++ app/(root)/posts/createpost/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import React from 'react'
+import { CreatePost } from '../../componets/CreatePost'
+
+
+export function page(){
+  return (
+    <CreatePost/>
+  )
+}
+
+export default page
