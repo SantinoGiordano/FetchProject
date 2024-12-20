bf97c7205b331bc5c0621a0281f2b4c090a8d9d9
SantinoGiordano <santinogiordano13@gmail.com>
2024-12-19 23:35:38 -0500

    added it to page

--- app/(root)/posts/page.tsx
+++ app/(root)/posts/page.tsx
@@ -1,8 +1,11 @@
+'use client'
+
 import React from 'react'
+import { CreatePost } from '../componets/CreatePost'
 
 const page = () => {
   return (
-    <div>page</div>
+    <CreatePost/>
   )
 }
 
