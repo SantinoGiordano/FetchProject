a7ff1e0629210280c4d20462d516770d82d3d893
SantinoGiordano <santinogiordano13@gmail.com>
2024-12-21 10:45:36 -0500

    had to put this inside of [id] so the params rould be stored

---
+++ app/api/(post backend)/getPost/[id]/route.ts
@@ -0,0 +1,28 @@
+
+import Userpost from '@/app/model/Userpost';
+import dbConnect from '@/utils/db';
+import { NextResponse } from 'next/server';
+import React from 'react'
+
+export async function GET(request: Request, { params }: { params: { id?: string } }) {
+    if (!params || !params.id) {
+        return new Response('Missing ID', { status: 400 });
+    }
+
+    const { id } = params;
+    try {
+        await dbConnect
+        const post = await Userpost.findById(id)
+        console.log(post)
+    if (!post) {
+          return NextResponse.json({ message: "Post not found" }, { status: 404 });
+        }
+    
+        // Return the account data as JSON
+        return NextResponse.json(post, { status: 200 });
+      } catch (error) {
+        console.error(error);
+        return NextResponse.json({ message: "Error fetching post" }, { status: 500 });
+      }
+    };
+
