619ce06fde258a9f1b7687b1f3889788d0607de6
SantinoGiordano <santinogiordano13@gmail.com>
2024-12-25 19:06:03 -0500

    created template header for home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,16 @@
-
 // import Register from "./componets/Register";
 
-
 export default async function Home() {
-  return(
-<div>
-  Hello
-</div>
-  )
+  return (
+    <div className="pt-52 flex items-center justify-center">
+      <div className="text-3xl text-white font-semibold bg-green-400 text-center rounded-md p-10">
+        Welcome
+        <div className="text-xl font-extralight rounded-md p-5 bg-white text-black">
+        <p>See what other poeple are up too</p>
+        <p>Share your chats and blogs with eveyone</p>
+        <p>Like and view there profiles</p>
+        </div>
+      </div>
+    </div>
+  );
 }
